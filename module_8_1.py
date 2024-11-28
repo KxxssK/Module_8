abdb845315a2870c498191497d489207db4003be
KxxssK <Kxxssk@yandex.ru>
2024-11-28 15:04:50 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,13 @@
+def add_everything_up(a, b):
+    try:
+        c = a + b
+    except TypeError:
+        c = f'{a}{b}'
+    else:
+        return round(c, 3)
+    return c
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
